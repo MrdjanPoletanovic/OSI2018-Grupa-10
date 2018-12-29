41a4e3f7e14904597d843048e4db27f7244a7b72
SlavisaStojakovic <45766892+SlavisaStojakovic@users.noreply.github.com>
2018-12-29 15:00:25 +0100

    Add files via upload
    Igra Pogadjanje broja

---
+++ broj.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+
+//#define jeste true
+//#define nije false
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void pogodiBroj();
+
