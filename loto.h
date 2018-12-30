fb4962da4a96ba4449514d757994a71483d9cc69
MrdjanPoletanovic <45563557+MrdjanPoletanovic@users.noreply.github.com>
2018-12-30 12:49:20 +0100

    Revert "Merge branch 'master' of https://github.com/MrdjanPoletanovic/OSI2018-Grupa-10"
    This reverts commit 4168654aedb62d825e3665120064d86a15b7507b, reversing
    changes made to ae8f74828ee6b21730897e03e2a19343e12061e7.

--- loto.h
+++ loto.h
@@ -5,11 +5,8 @@
 void izaberi(int izabrani[], int niz[]);
 void izvuci(int izvuceni[], int niz[], int n);
 void prikazi_izvlacenje(int izvuceni[]);
-int provjera(int izvuceni[], int izabrani[], bool is_namjestanje=false);
+int provjera(int izvuceni[], int izabrani[]);
 void loto(int& stanje, int& dobitak, int& gubitak, int& igranje);
 bool provjeri_broj(int izabrani[], int i);
 bool check_input(std::string&, int izabrani[], int niz[]);
 void loto(int& stanje, int& dobitak, int& gubitak, int& igranje);
-bool check_namjestanje(int dobitak, int gubitak, int& igranje, int& redni);
-int get_optimalno(int dobitak, int gubitak);
-void namjesti(int izabrani[], int izvuceni[], int niz[], int optimalno);
