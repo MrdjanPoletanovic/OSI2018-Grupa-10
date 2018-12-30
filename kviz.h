97c1b4cf09f684bf58120a724dc0e5ff2a9e178f
Njegoš Dukić <45604026+NjegosDukic@users.noreply.github.com>
2018-12-30 19:51:35 +0100

    Update kviz.h
    Azuriranje definicije funkcije "pritisakNaDugme".

--- kviz.h
+++ kviz.h
@@ -13,6 +13,6 @@ typedef struct pitanja_odgovori
 void pisanjePitanjaCSV();
 int stoi (std::string);
 void pisiPitanjeIOdgovore(PITANJA, int);
-bool pritisakNaDugme (DWORD, char);
+bool pritisakNaDugme (DWORD, char&);
 int kviz();
 
