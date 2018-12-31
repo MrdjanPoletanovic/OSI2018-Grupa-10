04f88bbbd643178d62b447f36bcadb755d356930
SlavisaStojakovic <45766892+SlavisaStojakovic@users.noreply.github.com>
2018-12-31 12:09:14 +0100

    Provjera uspjesnosti prijave na sistem

--- provjeraPrijave.cpp
+++ provjeraPrijave.cpp
@@ -6,13 +6,12 @@
 bool provjera(std::string &userName, std::string &password)
 {
     std::ifstream dataBase;
-    dataBase.open("korisnici.csv");          //otvaranje csv fajla pod imenom "korisnici"; promijeniti naziv fajla
+    dataBase.open("nalozi.csv");          //otvaranje csv fajla pod imenom "nalozi"; promijeniti naziv fajla
     while(dataBase.good())
     {
         std::string _name, _password, ignore;
         getline(dataBase, _name, ',');
-        getline(dataBase, _password, ',');
-        getline(dataBase, ignore, '\n');     // ignorisanje ostatka reda; potrebni samo username i password
+        getline(dataBase, _password, '\n');     // pretpostavka da u tabeli postoje samo 2 kolone: username i password
         if(userName.compare(_name)==0 && password.compare(_password)==0)
             return true;
     }
