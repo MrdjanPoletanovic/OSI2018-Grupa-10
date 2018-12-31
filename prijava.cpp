23f4ddc7605f8dc129afad119f11dfc8b04faffd
SlavisaStojakovic <45766892+SlavisaStojakovic@users.noreply.github.com>
2018-12-31 12:47:28 +0100

    Prijava na sistem : Dodati komentari

--- prijava.cpp
+++ prijava.cpp
@@ -14,8 +14,8 @@ void prijava()
     do
     {
         x=getch();
-        if(x=='\r') break;
-        if(x=='\b')
+        if(x=='\r') break;                                  //unos lozinke zavrsava pritiskom tipke "Enter"
+        if(x=='\b')                                         //omogucavanje ponistavanje poslednjeg karaktera kad se pritisne "Backspace"
         {
            password=password.substr(0,password.length()-1);
            std::cout<<"\b"<<" \b";
