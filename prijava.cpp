68105160a61e3cba289fbf91fc8a5c811a242036
SlavisaStojakovic <45766892+SlavisaStojakovic@users.noreply.github.com>
2019-01-01 15:18:14 +0100

    Prijava na sistem
    dodan uslov za sprecavanje brisanja labele "Password: " pritiskom tipke "Backspace"

--- prijava.cpp
+++ prijava.cpp
@@ -17,8 +17,11 @@ void prijava()
         if(x=='\r') break;                                  //unos lozinke zavrsava pritiskom tipke "Enter"
         if(x=='\b')                                         //omogucavanje ponistavanje poslednjeg karaktera kad se pritisne "Backspace"
         {
-           password=password.substr(0,password.length()-1);
-           std::cout<<"\b"<<" \b";
+            if(password.length()>0)                         //sprecavanje brisanja labele "Password: " pritiskom tipke "Backspace"
+                {
+                    password=password.substr(0,password.length()-1);
+                    std::cout<<"\b"<<" \b";
+                }
         }
         else
         {
