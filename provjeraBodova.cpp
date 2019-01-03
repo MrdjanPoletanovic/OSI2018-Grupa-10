9ee09563e6d7bff433c9daf4b068e349c0fcbfc6
MrdjanPoletanovic <45563557+MrdjanPoletanovic@users.noreply.github.com>
2019-01-03 10:59:57 +0100

    Update provjeraBodova.cpp

--- provjeraBodova.cpp
+++ provjeraBodova.cpp
@@ -6,7 +6,7 @@ bool provjeraBodova(const std::string &igra, const int &bodovi)
     if(igra.compare("broj")==0 ) 
         return true;
     else if (igra.compare("kviz")==0 && bodovi>=50)
-    	retutn true;
+    	return true;
     else if(igra.compare("loto")==0 && bodovi>=100)
         return true;
     else if(igra.compare("poker")==0 && bodovi>=5)
