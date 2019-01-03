f0be30a5cb990c5b80495f9f80266ad645aaed0a
Bojan Bulatovic <bulatb@hotmail.com>
2019-01-03 14:33:59 +0100

    Spojene funkcije u jedan fajl

---
+++ prijava_registracija.cpp
@@ -0,0 +1,128 @@
+#include "prijava_registracija.h"
+#include <iostream>
+#include <conio.h>
+#include <cctype>
+#include <string>
+#include <fstream>
+
+
+bool logIn()
+{
+    std::cout<<"User name: ";
+    std::string name;
+    std::getline(std::cin, name);
+    std::cout<<"Password: ";
+    std::string password;
+    char x;
+    do
+    {
+	    x=getch();
+	    if(x=='\r') break;                                  //unos lozinke zavrsava pritiskom tipke "Enter"
+	    if(x=='\b')                                         //omogucavanje ponistavanje poslednjeg karaktera kad se pritisne "Backspace"
+	    {
+		    if(password.length()>0)                         //sprecavanje brisanja labele "Password: " pritiskom tipke "Backspace"
+		    {
+			    password=password.substr(0,password.length()-1);
+			    std::cout<<"\b"<<" \b";
+		    }
+	    }
+	    else
+	    {
+		    std::cout<<"*";
+		    password+=x;
+	    }
+    }
+    while(x!='\r');
+    if(provjera(name, password)) return true;
+    else return false;
+}
+
+
+void signUp()
+{
+	std::string korisnickoIme, lozinka;
+	bool controlIme, controlLozinka;
+
+	std::cout << "*** Molim unesite korisnicko ime koristeci alfanumericke znakove i simbole tacka (.) ili donja crta (_)";
+	std::cout << std::endl << "*** Molim unesite sigurnu lozinku koristeci alfanumericke znakove." << std::endl;
+
+	do
+	{
+		controlIme = true;
+		controlLozinka = true;
+
+		std::cin.sync(); // Ciscenje ulaznog bafera.
+		std::cout << "\nMolim unesite korisnicko ime: ";
+		std::getline(std::cin, korisnickoIme);
+
+		for (int j = 0; j < (int)korisnickoIme.length(); j++) // Provjera da li unijeto korisnicko ime sadrzi nedozvoljene karaktere.
+			if (!(isalpha(korisnickoIme.at(j)) || isdigit(korisnickoIme.at(j)) || korisnickoIme.at(j) == '_' || korisnickoIme.at(j) == '.'))
+				controlIme = false;
+
+		std::cin.sync(); // Ciscenje ulaznog bafera.
+		std::cout << "Molim unesite lozinku: ";
+		std::getline(std::cin, lozinka);
+
+		for (int j = 0; j < (int)lozinka.length(); j++)  // Provjera da li unijeta lozinka sadrzi nedozvoljene karaktere.
+			if (!(isalpha(lozinka.at(j)) || isdigit(lozinka.at(j))))
+				controlLozinka = false;
+
+		std::cout << std::endl;
+
+		if (!controlIme)
+			std::cout << "Unijeli ste ime u pogresnom formatu.\n";
+
+		if (!controlLozinka)
+			std::cout << "Unijeli ste lozinku u pogresnom formatu.\n";
+	}
+	while (!controlIme || !controlLozinka);
+
+	std::ifstream datotekaSaNalozima; // Otvaranje datoteke za iscitavanje naloga kao "Input File Stream".
+	datotekaSaNalozima.open("nalozi.csv");
+
+	std::string compareUsername, demPassword;
+	bool control = true;
+
+	if (datotekaSaNalozima.is_open())
+		// Poredjenje naloga iz fajla sa unijetim nalogom.
+		while(!datotekaSaNalozima.eof())
+		{
+			getline(datotekaSaNalozima, compareUsername, ',');
+			getline(datotekaSaNalozima, demPassword);
+
+			if (!korisnickoIme.compare(compareUsername))
+			{
+				std::cout << "\nKorisnicko ime vec postoji.\n";
+				control  = false;
+				break;
+			}
+		}
+	// Zatvaranje datoteke u modu za pisanje.
+	datotekaSaNalozima.close();
+
+	// Otvaranje datoteke u modu za upisivanje "Output File Stream" u "Append" modu.
+	std::ofstream datotekaSaNalozimaUpis;
+	datotekaSaNalozimaUpis.open("nalozi.csv", std::ios::app);
+
+	if (control)
+		datotekaSaNalozimaUpis << korisnickoIme << ',' << lozinka << '\n'; // Formatiran upis korisnickog imena i lozinke u fajl.
+
+	datotekaSaNalozimaUpis.close();
+	return;
+}
+
+
+bool provjera(std::string &userName, std::string &password)
+{
+    std::ifstream dataBase;
+    dataBase.open("nalozi.csv");          //otvaranje csv fajla pod imenom "nalozi"; po potrebi promijeniti naziv fajla
+    while(dataBase.good())
+    {
+        std::string _name, _password;
+        getline(dataBase, _name, ',');
+        getline(dataBase, _password, '\n');     // pretpostavka da u tabeli postoje samo 2 kolone: username i password
+        if(userName.compare(_name)==0 && password.compare(_password)==0)
+            return true;
+    }
+    return false;
+}
--- prijava_registracija.h
+++ prijava_registracija.h
@@ -1,4 +1,6 @@
 #pragma once
-#include <iostream>
+#include <string>
 
+bool logIn();
+void signUp();
 bool provjera(std::string&, std::string&);
