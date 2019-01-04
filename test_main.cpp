7597ef09733935cab12cd518cae1fb6124e9ab0b
MrdjanPoletanovic <45563557+MrdjanPoletanovic@users.noreply.github.com>
2019-01-04 10:46:42 +0100

    Update test_main.cpp
    Za potrebe testiranje napravljene dodatne pogodnosti

--- test_main.cpp
+++ test_main.cpp
@@ -5,20 +5,28 @@
 
 int main()
 {
+	int control;
+	std::string control_string;
+	std::cout << "Da li zelite da se registrujete ili prijavite ( 1 / 0 )?";
+	std::getline(std::cin, control_string);
+	control = std::stoi(control_string);
+	if(control)
+		signUp();
 	if(logIn())
 	{
 		Igrac i("ime", "password");
 		i.igraj_broj();
 		i.printStatBroj();
-		//i.igraj_loto();
-		//i.igraj_poker();
-		//i.printStatLoto();
-		//i.printStatPoker();
+		i.igraj_loto();
+		i.igraj_poker();
+		i.printStatLoto();
+		i.printStatPoker();
 		std::cout << "Dobitak: " << i.getDobitak() << std::endl;
 		std::cout << "Gubitak: " << i.getGubitak() << std::endl;
 		std::cout << "Stanje: " << i.getStanje() << std::endl;
 	}
 	else
-		std::cout << "Ne mozete" << std::endl;
+		std::cout << "Ne mozete igrati!" << std::endl;
+	getchar();
 	return 0;
 }
